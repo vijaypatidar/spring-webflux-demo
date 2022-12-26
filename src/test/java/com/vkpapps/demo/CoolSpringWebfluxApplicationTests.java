6112a6e146a3c7e51abeedc3744354c4bbc165fc
Vijay Patidar <vramotiya@gmail.com>
2022-12-26 18:56:58 +0530

    Fixed sonal smell

--- src/test/java/com/vkpapps/demo/CoolSpringWebfluxApplicationTests.java
+++ src/test/java/com/vkpapps/demo/CoolSpringWebfluxApplicationTests.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import software.amazon.awssdk.utils.ImmutableMap;
 
 @SpringBootTest
 class CoolSpringWebfluxApplicationTests {
