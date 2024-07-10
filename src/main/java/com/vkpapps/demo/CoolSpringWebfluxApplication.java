3bde6feca1f1df4df11ff59a9f261a42caad59ac
Vijay Patidar <41512842+vijaypatidar@users.noreply.github.com>
2024-07-10 06:38:08 -0700

    Proto (#10)
    Co-authored-by: vijay <vramotiya@gmail.com>

--- src/main/java/com/vkpapps/demo/CoolSpringWebfluxApplication.java
+++ src/main/java/com/vkpapps/demo/CoolSpringWebfluxApplication.java
@@ -1,11 +1,15 @@
 package com.vkpapps.demo;
 
+import com.test.Person;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.web.reactive.config.EnableWebFlux;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 @SpringBootApplication
 @EnableWebFlux
@@ -15,8 +19,13 @@ import org.springframework.web.reactive.config.EnableWebFlux;
 @ConfigurationPropertiesScan
 public class CoolSpringWebfluxApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(CoolSpringWebfluxApplication.class, args);
+  public static void main(String[] args) throws IOException {
+    Person person = Person.newBuilder().setEmail("vijay@test.com").setName("Vijay h j Patidar").setId(71).build();
+
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream(5000);
+    person.writeTo(outputStream);
+    System.out.println("Person:"+outputStream);
+//    SpringApplication.run(CoolSpringWebfluxApplication.class, args);
   }
 
 }
