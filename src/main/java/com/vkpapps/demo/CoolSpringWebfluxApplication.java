a6325b1e67226c8c4d38cd1604b735d62e37b25d
vijay <vramotiya@gmail.com>
2024-07-10 19:11:18 +0530

    Fixed main

--- src/main/java/com/vkpapps/demo/CoolSpringWebfluxApplication.java
+++ src/main/java/com/vkpapps/demo/CoolSpringWebfluxApplication.java
@@ -1,6 +1,5 @@
 package com.vkpapps.demo;
 
-import com.test.Person;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
@@ -8,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 @SpringBootApplication
@@ -19,13 +17,8 @@ import java.io.IOException;
 @ConfigurationPropertiesScan
 public class CoolSpringWebfluxApplication {
 
-  public static void main(String[] args) throws IOException {
-    Person person = Person.newBuilder().setEmail("vijay@test.com").setName("Vijay h j Patidar").setId(71).build();
-
-    ByteArrayOutputStream outputStream = new ByteArrayOutputStream(5000);
-    person.writeTo(outputStream);
-    System.out.println("Person:"+outputStream);
-//    SpringApplication.run(CoolSpringWebfluxApplication.class, args);
+  public static void main(String[] args) {
+    SpringApplication.run(CoolSpringWebfluxApplication.class, args);
   }
 
 }
